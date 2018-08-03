a0e72177c13d3c51ab59cb8728886ab62996f045
bruno.allison <bruno.allison@live.com>
2018-08-03 00:46:37 -0400

    WIP

--- app/src/main/java/com/book/app/LoginUsuario.java
+++ app/src/main/java/com/book/app/LoginUsuario.java
@@ -4,10 +4,21 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.book.app.api_service.AlfabrinqueService;
 import com.book.app.util.UtilitarioUI;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @SuppressLint("Registered")
 public class LoginUsuario extends AppCompatActivity{
+
+    Retrofit retrofit =  new Retrofit.Builder()
+            .baseUrl(AlfabrinqueService.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    AlfabrinqueService service = retrofit.create(AlfabrinqueService.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/book/app/api_service/AlfabrinqueService.java
@@ -0,0 +1,28 @@
+package com.book.app.api_service;
+
+import com.book.app.pojo_api.Login;
+import com.book.app.pojo_api.Palavra;
+import com.book.app.pojo_api.Usuario;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface AlfabrinqueService {
+    public static final String BASE_URL = "http://localhost:8000/api/";
+
+    @POST("registrar")
+    boolean registrar();
+
+    @POST("login")
+    Call<Usuario> login(@Body Login login);
+
+    @GET("buscarPalavra")
+    Call<Palavra> buscarPalavra(@Header("Authorization") String token);
+
+    @POST("enviarPalavra")
+    boolean enviarPalavra();
+
+}
